89ea3219a11f89389802a5ed20ba636387c6d4bb
strep688 <113553291+strep688@users.noreply.github.com>
2023-10-13 22:09:06 -0600

    Swapped arrow library.

--- alberta.js
+++ alberta.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
-
 const supabaseUrl = 'https://upqeefkzpseklypzxhjt.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwcWVlZmt6cHNla2x5cHp4aGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyMjM5OTcsImV4cCI6MjAxMjc5OTk5N30.TLiVkIy9cIw4dFqNz3gvGknmr-4nusL60Uxp7wADgKw'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+app.use(express.static('arrows'))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/web/index.html'));
 })
@@ -26,6 +26,13 @@ app.get('/courses', async (req, res) => {
 })
 
 app.get('/majors', async (req, res) => {
-    const { data, error } = await supabase.from('course').select()
+    const { data, error2 } = await supabase.from('majors').select()
     res.send(data)
 })
+
+app.get('/universities', async (req, res) => {
+    const { data, error3 } = await supabase.from('university').select()
+    res.send(data)
+})
+
+
