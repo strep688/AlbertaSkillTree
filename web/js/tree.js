602011cd2d2c1c4da11a4c6bf85ac222fa31266d
strep688 <113553291+strep688@users.noreply.github.com>
2023-10-14 04:20:16 -0600

    \=0A

--- web/js/tree.js
+++ web/js/tree.js
@@ -11,16 +11,27 @@ function hideContent() {
 }
 
 function toggleContent() {
+    
+    const form = document.getElementById("form");
+    form.addEventListener("sumbmit", (e) => {
+        e.preventDefault();
+        console.log(Object.fromEntries(new FormData(e.target)));
+        console.log(Object.fromEntries(new FormData(form)));
 
-    var x = document.getElementById("finished-courses").value;
+    });
 
-    console.log(x);
+
+    var data1 = [];
+    
+
+    console.log(data1);
+    console.log("gaming");
     hideContent();
     hideForm();
 }
 
 function hideForm() {
-    {
+    {   
         var x = document.getElementById('userInput');
 
         if (x.style.display === "none") {
@@ -32,23 +43,93 @@ function hideForm() {
 }
 
 
+var z = 3;
 
 window.onload = myMain;
 
 function myMain() {
-    document.getElementById("content").onclick = buton;
+  document.getElementById("content").onclick = buton;
 }
 
 function buton(e) {
-    if (e.target.tagName == 'BUTTON') {
-        toggleButton(e)
+  if (e.target.tagName == 'BUTTON') {
+    toggleButton(e)
+  }
+}
+
+function toggleButton(e){
+    if (e.target.style.backgroundColor == "lightblue"){
+        e.target.style.backgroundColor = "beige";
+    }else{
+        e.target.style.backgroundColor = "lightblue";
+        toggleCourseSkills(e);
     }
 }
 
-function toggleButton(e) {
-    if (e.target.style.backgroundColor == "lightblue") {
-        e.target.style.backgroundColor = "beige";
-    } else {
-        e.target.style.backgroundColor = "lightblue";
-    }
+function toggleCourseSkills(e){
+
+var name = e.target.id;
+console.log(name);
+console.log(z);
+switch(e){  
+    case Math1:
+        document.getElementById("Math10C").style.zIndex = z;
+    break;
+    case Math2:
+        document.getElementById("Math20-1").style.zIndex = z;
+    break;
+    case Math3:
+        document.getElementById("Math30-1").style.zIndex = z;
+    break;
+    case Eng1:
+        document.getElementById("English10-1").style.zIndex = z;
+    break;
+    case Eng2:
+        document.getElementById("English20-1").style.zIndex = z;
+    break;
+    case Eng3:
+        document.getElementById("English30-1").style.zIndex = z;
+    break;
+    case Soc1:
+        document.getElementById("Social10-1").style.zIndex = z;
+    break;
+    case Soc2:
+        document.getElementById("Social20-1").style.zIndex = z;
+    break;
+    case Soc3:
+        document.getElementById("Social30-1").style.zIndex = z;
+    break;
+    case Chem1:
+        document.getElementById("Chemistry10-1").style.zIndex = z;
+    break;
+    case Chem2:
+        document.getElementById("Chemistry20-1").style.zIndex = z;
+    break;
+    case Chem3:
+        document.getElementById("Chemistry30-1").style.zIndex = z;
+    break;
+    case Phys1:
+        document.getElementById("Physics10-1").style.zIndex = z;
+    break;
+    case Phys2:
+        document.getElementById("Physics20-1").style.zIndex = z;
+    break;
+    case Phys3:
+        document.getElementById("Physics30-1").style.zIndex = z;
+    break;
+    case Bio1:
+        document.getElementById("Biology10-1").style.zIndex = z;
+    break;
+    case Bio2:
+        document.getElementById("Biology20-1").style.zIndex = z;
+    break;
+    case Bio3:
+        document.getElementById("Biology30-1").style.zIndex = z;
+    break;
+    default:
+        
+    break;
 }
+    z++;
+}
+
